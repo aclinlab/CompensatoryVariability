f7841bb065f12357d756a84690a00da227dec63c
NadaYehia <43578602+NadaYehia@users.noreply.github.com>
2021-01-15 17:25:12 +0000

    Add files via upload (#1)
    * Add files via upload
    
    code necessary to create the results in Fig2,3, and S1.
    
    * Add files via upload
    
    * Delete dbiCalc.m
    
    * Delete VarDegradesPerf.m
    
    * Delete AngDist_Pw_Calc.m
    
    * Delete Calc_C.m
    
    * Delete KernelTesting_Ftheta.m
    
    * Delete KernelTesting.m
    
    * Delete dimInputCurrent.m
    
    * results on variability degrades performance
    
    Fig2,3,4,and S1

---
+++ AngDist_Pw_Calc.m
@@ -0,0 +1,41 @@
+function PW_AngDist=AngDist_Pw_Calc(Y)
+% y is reshaped: 2000 x odorsx num trials
+% y is rescaled from 0 to 1
+n=2000;
+numTrials=30;
+
+odors=size(Y,2)/numTrials;
+
+Y= reshape(Y,n,odors,numTrials);
+Y=rescale(Y);
+
+no_ods= size(Y,2);
+PW_AngDist=zeros(no_ods).*inf;
+
+for i=1:no_ods
+    
+    S1Centroid=mean(Y(:,i,:),3);
+    
+    Yreshaped= permute(Y(:,i,:),[1,3,2]);               
+    
+    for j=1:no_ods
+        
+        if(i~=j)
+        
+            S2Centroid=mean(Y(:,j,:),3);
+
+            Yjreshaped= permute(Y(:,j,:),[1,3,2]);
+
+            PW_AngDist(i,j)= (acos(( (S1Centroid'/norm(S1Centroid'))*(S2Centroid/norm(S2Centroid))) )/ (0.5*pi) ) ;
+        end
+        
+    end
+    
+    
+    
+end
+
+end
+
+
+
