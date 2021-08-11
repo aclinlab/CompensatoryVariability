92b66a10797c0e0a36462b30efbe0469f8698e1b
Andrew Lin <andrewclin@gmail.com>
2021-08-11 14:34:03 +0100

    fix indentation vardegrades

--- Variability_degrades_Perf/HomogenousModel_KernelTesting.m
+++ Variability_degrades_Perf/HomogenousModel_KernelTesting.m
@@ -15,43 +15,43 @@ odors= (size(YHomog,2))*numm;
 
 
 for trials=numtrainingSamples+1:numTrials
-
-                    for odour=1:(size(YHomog,2))
-
-                        
-
-                        z1H=WopHom(:,1)'*YHomog(:,odour,trials);
-                        z2H=WopHom(:,2)'*YHomog(:,odour,trials);
-                        
-                        %% prob. of success in the homogenous model 
-                        if ( ~ isempty(find(classAction1==odour)) )
-                            
-                            pr_action1H= 1/(1+exp(C*(z2H-z1H)));
-                            
-                            %if(pr_action1H>rand(1))
-                            p_raH=p_raH+(pr_action1H/odors);
-                            
-                            
-                        end
-
-                        if (  isempty((find(classAction1==odour))) )
-                            
-                            pr_action2H=  1/(exp(C*(z1H-z2H))+1);
-                            %if(pr_action2H>rand(1))
-                                p_raH=p_raH+(pr_action2H/odors);
-                            
-                            
-                        end
-                        
-                       
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                    end
+    
+    for odour=1:(size(YHomog,2))
+        
+        
+        
+        z1H=WopHom(:,1)'*YHomog(:,odour,trials);
+        z2H=WopHom(:,2)'*YHomog(:,odour,trials);
+        
+        %% prob. of success in the homogenous model
+        if ( ~ isempty(find(classAction1==odour)) )
+            
+            pr_action1H= 1/(1+exp(C*(z2H-z1H)));
+            
+            %if(pr_action1H>rand(1))
+            p_raH=p_raH+(pr_action1H/odors);
+            
+            
+        end
+        
+        if (  isempty((find(classAction1==odour))) )
+            
+            pr_action2H=  1/(exp(C*(z1H-z2H))+1);
+            %if(pr_action2H>rand(1))
+            p_raH=p_raH+(pr_action2H/odors);
+            
+            
+        end
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    end
 end
 
 
--- Variability_degrades_Perf/testingRandomModel_accuracy.m
+++ Variability_degrades_Perf/testingRandomModel_accuracy.m
@@ -1,43 +1,43 @@
 function [p_ra]=testingRandomModel_accuracy(C,Wop,classAction1,numTrials,numtrainingSamples,Y)
 
-              
+
 numm=numTrials-numtrainingSamples;
 p_ra=0;
 
-i=1; 
+i=1;
 
 odors= (size(Y,2))*numm;
 
 for trials=numtrainingSamples+1:numTrials
-
-                    for odour=1:(size(Y,2))
-
-                        z1=Wop(:,1)'*Y(:,odour,trials);
-                        z2=Wop(:,2)'*Y(:,odour,trials);
-
-                       
-                        
-                        if (~ isempty(find(classAction1==odour)) )
-                            
-                            
-                            pr_action1=  1/(1+exp(C*(z2-z1)));
-                            
-                            p_ra=p_ra+(pr_action1/odors);
-
-                        end
-
-                        if ( isempty((find(classAction1==odour))) )
-                            
-                            
-                            pr_action2=  1/(exp(C*(z1-z2))+1);
-                            
+    
+    for odour=1:(size(Y,2))
+        
+        z1=Wop(:,1)'*Y(:,odour,trials);
+        z2=Wop(:,2)'*Y(:,odour,trials);
+        
+        
+        
+        if (~ isempty(find(classAction1==odour)) )
             
-                            p_ra=p_ra+(pr_action2/odors);
-
-                        end
-
-                        
-                    end
+            
+            pr_action1=  1/(1+exp(C*(z2-z1)));
+            
+            p_ra=p_ra+(pr_action1/odors);
+            
+        end
+        
+        if ( isempty((find(classAction1==odour))) )
+            
+            
+            pr_action2=  1/(exp(C*(z1-z2))+1);
+            
+            
+            p_ra=p_ra+(pr_action2/odors);
+            
+        end
+        
+        
+    end
 end
 
 
